42cb4dd50abe1184906a30a2953617e981dd1f99
myumoon <MYU@player1.(none)>
2014-08-10 15:55:51 +0900

    add types

---
+++ nk/include/nk/types.h
@@ -0,0 +1,51 @@
+//=============================================================================
+/*!	@file	types.h
+
+	@brief	Œ^’è‹`
+
+	@author	ryunosuke ide
+	@date	2012.06.08
+*/
+//=============================================================================
+
+#ifndef NK_TYPES_H
+#define NK_TYPES_H
+
+
+#if defined(WIN32)
+	typedef char						s8;
+	typedef unsigned char				u8;
+	typedef short						s16;
+	typedef unsigned short				u16;
+	typedef int							s32;
+	typedef	unsigned					u32;
+	typedef	long long					s64;
+	typedef	unsigned long long	 		u64;
+	typedef	float						f32;
+	typedef	double						f64;
+
+#elif defined(_WIN64)
+	typedef char						s8;
+	typedef unsigned char				u8;
+	typedef short						s16;
+	typedef unsigned short				u16;
+	typedef int							s32;
+	typedef	unsigned					u32;
+	typedef	long						s64;
+	typedef	unsigned long		 		u64;
+	typedef	float						f32;
+	typedef	double						f64;
+
+#endif
+
+#if defined(_WINDOWS)
+	#define NK_PROCESS_INSTANCE			HINSTANCE
+	#define NK_WINDOW_HANDLE			HWND
+#else
+	#define NK_PROCESS_INSTANCE			u32
+	#define NK_WINDOW_HANDLE			u32
+#endif
+
+
+
+#endif  // NK_TYPES_H
